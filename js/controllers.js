dfd1cae8b0bd0ecccff169ade7914fb87d85b0c9
yosef <csyosef@gmail.com>
2014-11-26 15:32:44 +0200

    add filter works from select option area (have duplicated results-should fix)

--- js/controllers.js
+++ js/controllers.js
@@ -6,6 +6,22 @@ var adminApp = angular.module('adminApp', []);
 
 adminApp.controller('AdminListCtrl', function($scope) {
     $scope.users = [
-        {name: 'John',age:20},{name: 'Ben',age:15},{name: 'Dan',age:25}
+        {name: 'John',age:20},{name: 'Ben',age:15},{name: 'Robert',age:15},{name: 'Dan',age:25},{name: 'Dan',age:30}
     ];
 });
+
+/**
+adminApp.filter('reduceByName', function () {
+    return reduceBy(items,'name',choosen.name);
+});
+
+function reduceBy(items,key,value) {
+    var filtered = [];
+    for (var i = 0; i < items.length; i++) {
+        if(value===items[i][key]){
+            filtered.push(items[i]);
+        }
+    }
+    return filtered;
+};
+ */
